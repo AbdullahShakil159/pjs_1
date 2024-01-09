097f1ab4741bf2a42cfb4f245e51bd1c0f29ecee
asp <asp@aptech.com>
2024-01-09 20:51:40 +0500

    third commit

---
+++ second.js
@@ -0,0 +1,39 @@
+let mydata = {
+    name : "Peter",
+    city : "Karachi",
+    age : "1000",
+    grade : "c",
+};
+
+console.log(mydata["city"]);
+console.log(mydata["age"]);
+
+console.log(mydata.salary);
+console.log(mydata.grade);
+console.log(mydata);
+
+for(let i in mydata){
+    console.log(i,mydata[i])
+}
+
+let a = 45;
+let b = 75;
+
+console.log("The sum of a=b is :",a+b);
+console.log("The sum of a-b is :",a-b);
+console.log("The sum of a*b is :",a*b);
+console.log("The sum of a/b is :",a/b);
+console.log("The sum of a%b is :",a%b);
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+
+
