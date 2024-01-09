b4cf82b8dc381a38509ddc561adaeb8f105544e2
asp <asp@aptech.com>
2024-01-09 20:16:40 +0500

    second commit

---
+++ first.js
@@ -0,0 +1,34 @@
+console.log(" Welcome To PJS");
+console.log("Hello World");
+
+name = "Mike";
+age = 45;
+isCnic = false;
+x=undefined;
+y=null;
+console.table("name,age,isCnic,x,y");
+console.log(typeof(name))
+console.log(typeof(isCnic))
+console.log(typeof(x))
+console.log(typeof(age))
+
+var city = "Karachi";
+console.log(city);
+city = "lahore";
+console.log(city);
+var city = "peshawar"
+console.log(city);
+
+
+let ages = 17;
+console.log(ages);
+ages = 18
+console.log(ages);
+
+const pie =3.14
+console.log(pie);
+
+
+
+
+
